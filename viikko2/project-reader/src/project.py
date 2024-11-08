4623ffe700382575c7bd7173e78a0fbc6ae70f64
Emilia <koponen.noora.emilia@gmail.com>
2024-11-08 22:17:35 +0200

    Poetry

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,7 +1,9 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
@@ -9,9 +11,18 @@ class Project:
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
+        newline = "\n"
         return (
-            f"Name: {self.name}"
-            f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"Name: {self.name}" 
+            f"\nDescription: {self.description or '-'}" 
+            f"\nLicense: {self.license}" 
+            f"\n"
+            f"\nAuthors:\n"
+            f"{newline.join(f'- {key}' for key in self.authors)}"+
+            f"\n"
+            f"\nDependencies:\n" +
+            f"{newline.join(f'- {key}' for key, value in self.dependencies.items())}" +
+            f"\n"
+            f"\nDevelopment dependencies:\n" +
+            f"{newline.join(f'- {key}' for key, value in self.dev_dependencies.items())}"
         )
